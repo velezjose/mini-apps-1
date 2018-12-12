a6fa28e1dcde4d69d788b05aac22ed506fa4d567
Jose Velez <josevelezq7@gmail.com>
2018-12-11 17:36:27 -0800

    Make server serve the client/ files

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,17 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 8080;
 
+// This is the same thing as:  app.use('/', express.static('client'));
+// It serves all files in the client/ directory.
+app.use(express.static('client'));
+app.use(bodyParser.json({type: '*/*'}));
 
+app.post('/', (req, res) => {
+  debugger;
+  // res.write(req.body);
+  res.send();
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}.`));
