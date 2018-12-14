aac71084c0ee0d05295c4856e8128fdaf0065131
Jose Velez <josevelezq7@gmail.com>
2018-12-13 16:41:48 -0800

    Configure server to serve public directory

--- challenge_4/server.js
+++ challenge_4/server.js
@@ -1,2 +1,8 @@
+const express = require('express');
+const app = express();
+const host = 'localhost';
+const port = 3000;
 
+app.use(express.static('public'));
 
+app.listen(port, host, () => `Listening to requests on port ${host}:${port}.`);
