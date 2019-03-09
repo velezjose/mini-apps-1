363d03c14bb8b3384a233720441e4f5cffd879cd
Jose Velez <josevelezq7@gmail.com>
2019-03-08 23:41:21 -0800

    Complete the basic requirements to make Connect Four work
    - Get checkDiagonals to work
    - Disallow pieces to be placed on the board when a column is full
    - Add some styling to the game board
    - Refactor some methods into a new directory utils/
    - Comment out all POST routes to the MySQL database

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -1,29 +1,7 @@
 import React from 'react';
 import Table from './Table.jsx';
 
-
-const rows = 6;
-const columns = 7;
-
-const getInitialTable = () => {
-  let initialTable = [];
-  for (let i = 0; i < rows; i += 1) {
-    initialTable.push([]);
-    for (let j = 0; j < columns; j += 1) {
-      initialTable[i].push(0);
-    }
-  }
-  return initialTable;
-}
-
-const getColIndexesToFill = () => {
-  let colIndexesToFill = [];
-  for (let i = 0; i < columns; i += 1) {
-    colIndexesToFill.push(rows - 1);
-  }
-  return colIndexesToFill;
-};
-
+import { getInitialTable, getColIndexesToFill } from '../utils/helpers.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -32,76 +10,83 @@ class App extends React.Component {
     this.state = {
       table: getInitialTable(),
       columnFillTracker: getColIndexesToFill(),
-      player1: true,
+      player1sTurn: true,
       hasWon: {
         someone: false,
         player1: false
       } 
     };
+
+    this.onCircleClickHandler = this.onCircleClickHandler.bind(this);
+    this.checkWinningConditions = this.checkWinningConditions.bind(this);
+    this.checkRow = this.checkRow.bind(this);
+    this.checkColumn = this.checkColumn.bind(this);
+    this.checkDiagonals = this.checkDiagonals.bind(this);
+    this.checkMajorDiagonal = this.checkMajorDiagonal.bind(this);
+    this.checkMinorDiagonal = this.checkMinorDiagonal.bind(this);
+    this.restartGame = this.restartGame.bind(this);
   }
 
   onCircleClickHandler(event) {
-    if (this.state.hasWon.someone) {
-      return;
-    }
+    if (this.state.hasWon.someone) return;
 
     let colIndex = Number(event.target.id.charAt(1))
     let nextRowIndex = this.getNextRowIndex(colIndex);
 
+    // If nextRowIndex is null, means that there is no nextRowIndex. In other words, the
+    // row is full and can't be filled any more.
+    if (nextRowIndex === null) return;
+
     let newTable = this.state.table.slice();
     // 1 = blue colored circle
     // 2 = red colored circle
-    newTable[nextRowIndex][colIndex] = this.state.player1 ? 1 : 2;
+    newTable[nextRowIndex][colIndex] = this.state.player1sTurn ? 1 : 2;
 
     // Check if Anyone has won
     let newHasWon = Object.assign({}, this.state.hasWon);
     this.checkWinningConditions(newHasWon, newTable, newTable[nextRowIndex][colIndex], nextRowIndex, colIndex);
 
     this.setState({
-      player1: !this.state.player1,
+      player1sTurn: !this.state.player1sTurn,
       table: newTable,
       hasWon: newHasWon
     }); 
-
   }
 
   checkWinningConditions(newHasWon, newTable, colorCode, rowIndex, colIndex) {
-    let someoneWon = this.checkRows(newTable, colorCode, rowIndex) || this.checkColumns(newTable, colorCode, colIndex); // this.checkDiagonals(newTable, colorCode)
+    let someoneWon = this.checkRow(newTable, colorCode, rowIndex) || this.checkColumn(newTable, colorCode, colIndex) || this.checkDiagonals(newTable, colorCode, colIndex, rowIndex);
 
     if (someoneWon) {
       newHasWon.someone = true;
-      newHasWon.player1 = this.state.player1;
-
-      this.postWinner(newHasWon.player1);
-    } 
+      newHasWon.player1 = this.state.player1sTurn;
+    }
   }
 
-  postWinner(player1Won) {
-    let whoWon = player1Won ? 'Player 1' : 'Player 2';
+  // postWinner(player1Won) {
+  //   let whoWon = player1Won ? 'Player 1' : 'Player 2';
 
-    let options = {
-      method: 'POST',
-      body: whoWon
-    };
+  //   let options = {
+  //     method: 'POST',
+  //     body: whoWon
+  //   };
 
-    fetch('/', options)
-      .then(() => {
-        console.log('posted nicely!');
-        setTimeout(() => this.setState({
-          table: getInitialTable(),
-          columnFillTracker: getColIndexesToFill(),
-          player1: true,
-          hasWon: {
-            someone: false,
-            player1: false
-          } 
-        }), 5000);
-      })
-  }
+  //   fetch('/', options)
+  //     .then(() => {
+  //       console.log('posted nicely!');
+  //       setTimeout(() => this.setState({
+  //         table: getInitialTable(),
+  //         columnFillTracker: getColIndexesToFill(),
+  //         player1: true,
+  //         hasWon: {
+  //           someone: false,
+  //           player1: false
+  //         }
+  //       }), 5000);
+  //     })
+  // }
 
 
-
-  checkRows(newTable, colorCode, rowIndex) {
+  checkRow(newTable, colorCode, rowIndex) {
     let row = newTable[rowIndex];
     let count = 0;
 
@@ -115,7 +100,7 @@ class App extends React.Component {
     return false;
   }
 
-  checkColumns(newTable, colorCode, columnIndex) {
+  checkColumn(newTable, colorCode, columnIndex) {
     let column = [];
     let count = 0;
 
@@ -133,33 +118,83 @@ class App extends React.Component {
     return false;
   }
 
-  // checkDiagonals(newTable, colorCode) {
-    
-  // }
+  checkDiagonals(newTable, colorCode, colIndex, rowIndex) {
+    return this.checkMajorDiagonal(newTable, colorCode, colIndex, rowIndex) || this.checkMinorDiagonal(newTable, colorCode, colIndex, rowIndex);
+  }
+
+  checkMajorDiagonal(newTable, colorCode, colIndex, rowIndex) {
+    let count = 0;
+
+    for (let r = rowIndex, c = colIndex; r < newTable.length && c < newTable[r].length; r += 1, c += 1) {
+      count = newTable[r][c] === colorCode ? (count + 1) : 0;
+      if (count >= 4) return true;
+    }
+
+    return false;
+  }
+
+  checkMinorDiagonal(newTable, colorCode, colIndex, rowIndex) {
+    let count = 0;
+
+    for (let r = rowIndex, c = colIndex; r < newTable.length && c < newTable[r].length; r += 1, c -= 1) {
+      count = newTable[r][c] === colorCode ? (count + 1) : 0;
+      if (count >= 4) return true;
+    }
+
+    return false;
+  }
 
   getNextRowIndex(colIndex) {
     let newColumnFillTracker = this.state.columnFillTracker.slice();
-    newColumnFillTracker[colIndex] = newColumnFillTracker[colIndex] > 0 ? (newColumnFillTracker[colIndex] - 1) : newColumnFillTracker[colIndex];
 
+    // If the newColumnFillTracker is the same as the old one, return null so the circleClickHandler
+    // function can know that it shouldn't proceed with changing the state because the entire column
+    // has already been filled and no need to proceed. Let the user click another circle.
+    if (newColumnFillTracker[colIndex] === -1) return null;
+
+    newColumnFillTracker[colIndex] -= 1;
     this.setState({
       columnFillTracker: newColumnFillTracker
     });
-    
+
     return this.state.columnFillTracker[colIndex];
   }
 
-  render() {
-    let color = this.state.player1 ? 'Red' : 'Blue';
+  restartGame() {
+    this.setState({
+      table: getInitialTable(),
+      columnFillTracker: getColIndexesToFill(),
+      player1sTurn: true,
+      hasWon: {
+        someone: false,
+        player1: false
+      },
+    });
+  }
 
-    return !this.state.hasWon.someone
-    ? (<div>
-        <Table table={this.state.table} onCircleClickHandler={this.onCircleClickHandler.bind(this)} />
-      </div>)
-    :
-      (<div>
-        <Table table={this.state.table} onCircleClickHandler={this.onCircleClickHandler.bind(this)} />
-        <p>{color} has won!</p>
-      </div>)
+  render() {
+    let color = this.state.player1sTurn ? 'Red' : 'Blue';
+    let styleColor = color === 'Red' ? 'red' : 'blue';
+    let canvasStyle = {fontFamily: 'Arial', background: 'lightgray', width: 500, left: 0, border: '1px solid black', borderRadius: 5};
+    let titleStyle = {padding: 15, marginLeft: 160};
+    let resetButtonStyle = {marginLeft: 200, marginBottom: 20, height: 25, width: 90, borderRadius: 5};
+    let winningFontStyle = {padding: 15, marginLeft: 185, fontSize: 16, color: styleColor};
+
+    return (
+      <div style={canvasStyle}>
+        <h2 style={titleStyle}>Connect Four</h2>
+
+        <Table table={this.state.table} onCircleClickHandler={this.onCircleClickHandler} />
+
+        {
+          !this.state.hasWon.someone ? null :
+            <>
+              <p style={winningFontStyle}>{color} has won!</p>
+              <button style={resetButtonStyle} onClick={this.restartGame}>Restart Game</button>
+            </>
+        }
+      </div>
+    );
   }
 }
 
