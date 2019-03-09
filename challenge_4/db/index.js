c85fcb73e2134ac9ff6074f59ea1d3c91ea44762
Jose Velez <josevelezq7@gmail.com>
2019-03-08 23:41:21 -0800

    Complete the basic requirements to make Connect Four work
    - Get checkDiagonals to work
    - Disallow pieces to be placed on the board when a column is full
    - Add some styling to the game board
    - Refactor some methods into a new directory utils/
    - Comment out all POST routes to the MySQL database

--- challenge_4/db/index.js
+++ challenge_4/db/index.js
@@ -3,17 +3,17 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'students',
+  password: 'student',
   database: 'player_stats'
 })
 
 connection.connect((err) => {
   if (err) {
-    console.log('some error occurred in connecting');
+    console.log('Some error occurred in connecting');
     return;
   }
 
-  console.log('Connected as id ' + connection.threadId);
+  console.log('MySQL: Connected as id ' + connection.threadId);
 });
 
 module.exports = connection;
